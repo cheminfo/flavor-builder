522c214f350140a1946fcb7c2eef3ad274d161a0
Daniel Kostro <kostro.d@gmail.com>
2015-09-15 14:21:41 +0200

    Use path.resolve to allow relative and absolute path in config

--- src/config.js
+++ src/config.js
@@ -12,17 +12,24 @@ try {
     process.exit(1);
 }
 
-
-config.couchurl = config.couchurl.replace(/\/$/, '');
-if (config.couchLocalUrl) config.couchLocalUrl = config.couchLocalUrl.replace(/\/$/, '');
-
-config.requrl = config.couchLocalUrl || config.couchurl;
-
 // Config given in the command line takes precendence
 for (var key in args) {
     config[key] = args[key];
 }
 
+// Check mandatory parameters
+var mandatory = ['dir', 'readConfig', 'cdn', 'direct', 'home', 'couchurl', 'couchLocalUrl', 'flavorUsername', 'couchDatabase'];
+for (var i = 0; i < mandatory.length; i++) {
+    if(config[mandatory[i]] === undefined) {
+        throw new Error(mandatory[i] + ' is mandatory');
+    }
+}
+
+config.couchurl = config.couchurl.replace(/\/$/, '');
+if (config.couchLocalUrl) config.couchLocalUrl = config.couchLocalUrl.replace(/\/$/, '');
+config.requrl = config.couchLocalUrl || config.couchurl;
+config.readConfig = path.resolve(config.readConfig || 'static/readConfig.json');
+config.dir = path.resolve(config.dir);
 config.flavorLayouts = config.flavorLayouts || {};
 
 exports = module.exports = config;
