ea3e2c57b4b6f4933ef8b8efbb45b802c184e59d
Michaël Zasso <targos@protonmail.com>
2021-06-24 10:56:06 +0200

    chore: update dependencies

--- src/config.js
+++ src/config.js
@@ -1,80 +1,101 @@
 'use strict';
 
-var args = require('minimist')(process.argv.slice(2));
-var path = require('path');
-var _ = require('lodash');
+const path = require('path');
 
+const _ = require('lodash');
+const minimist = require('minimist');
+
+const args = minimist(process.argv.slice(2));
 exports = module.exports = function (configArg) {
-    configArg = configArg || 'config.json';
-    if (typeof configArg === 'string') {
-        var configFile = path.resolve(configArg);
-        var config = require(configFile);
-    } else if (typeof configArg === 'object') {
-        // Make sure we have another reference
-        // To avoid the config being changed from
-        // outside during a build
-        config = _.cloneDeep(configArg);
-    } else {
-        throw new TypeError('Incorrect argument');
-    }
-    // Config given in the command line takes precendence
-    for (let key in args) {
-        config[key] = args[key];
-    }
+  configArg = configArg || 'config.json';
+  let config;
+  if (typeof configArg === 'string') {
+    let configFile = path.resolve(configArg);
+    config = require(configFile);
+  } else if (typeof configArg === 'object') {
+    // Make sure we have another reference
+    // To avoid the config being changed from
+    // outside during a build
+    config = _.cloneDeep(configArg);
+  } else {
+    throw new TypeError('Incorrect argument');
+  }
+  // Config given in the command line takes precendence
+  for (let key in args) {
+    config[key] = args[key];
+  }
 
-    // Check mandatory parameters
-    var mandatory = ['dir', 'cdn', 'direct', 'home', 'couchurl', 'couchLocalUrl', 'flavorUsername', 'couchDatabase', 'layouts', 'libFolder'];
-    for (var i = 0; i < mandatory.length; i++) {
-        if (config[mandatory[i]] === undefined) {
-            throw new Error(mandatory[i] + ' is mandatory');
-        }
+  // Check mandatory parameters
+  let mandatory = [
+    'dir',
+    'cdn',
+    'direct',
+    'home',
+    'couchurl',
+    'couchLocalUrl',
+    'flavorUsername',
+    'couchDatabase',
+    'layouts',
+    'libFolder',
+  ];
+  for (let i = 0; i < mandatory.length; i++) {
+    if (config[mandatory[i]] === undefined) {
+      throw new Error(`${mandatory[i]} is mandatory`);
     }
+  }
 
-    if (process.env.COUCHDB_USER) {
-        config.couchUsername = process.env.COUCHDB_USER;
-        config.couchPassword = process.env.COUCHDB_PASSWORD;
-    }
+  if (process.env.COUCHDB_USER) {
+    config.couchUsername = process.env.COUCHDB_USER;
+    config.couchPassword = process.env.COUCHDB_PASSWORD;
+  }
 
-    if (!config.revisionByIdPath) {
-        config.revisionByIdPath = path.resolve(__dirname, '../revisionById.json');
-    }
+  if (!config.revisionByIdPath) {
+    config.revisionByIdPath = path.resolve(__dirname, '../revisionById.json');
+  }
 
-    if (!config.md5Path) {
-        config.md5Path = path.resolve(__dirname, '../md5.json');
-    }
+  if (!config.md5Path) {
+    config.md5Path = path.resolve(__dirname, '../md5.json');
+  }
 
-    config.couchReqOptions = config.couchPassword ? {
+  config.couchReqOptions = config.couchPassword
+    ? {
         auth: {
-            user: config.couchUsername,
-            pass: config.couchPassword,
-            sendImmediately: true
-        }
-    } : {};
+          user: config.couchUsername,
+          pass: config.couchPassword,
+          sendImmediately: true,
+        },
+      }
+    : {};
 
-    config.couchurl = config.couchurl.replace(/\/$/, '');
-    if (config.rootUrl) {
-        config.rootUrl = config.rootUrl.replace(/\/$/, '');
+  config.couchurl = config.couchurl.replace(/\/$/, '');
+  if (config.rootUrl) {
+    config.rootUrl = config.rootUrl.replace(/\/$/, '');
+  }
+
+  if (config.couchLocalUrl) {
+    config.couchLocalUrl = config.couchLocalUrl.replace(/\/$/, '');
+  }
+  config.dir = path.resolve(config.dir);
+  config.flavorLayouts = config.flavorLayouts || {};
+  if (config.layouts) {
+    for (let key in config.layouts) {
+      config.layouts[key] = path.join(
+        __dirname,
+        '../layout',
+        config.layouts[key],
+      );
     }
+  }
 
-    if (config.couchLocalUrl) config.couchLocalUrl = config.couchLocalUrl.replace(/\/$/, '');
-    config.dir = path.resolve(config.dir);
-    config.flavorLayouts = config.flavorLayouts || {};
-    if (config.layouts) {
-        for (let key in config.layouts) {
-            config.layouts[key] = path.join(__dirname, '../layout', config.layouts[key]);
-        }
-    }
+  config.isSelfContained = function (flavor) {
+    if (!config.selfContained) return false;
+    else if (config.selfContained === true) return true;
+    else return config.selfContained[flavor];
+  };
 
-    config.isSelfContained = function (flavor) {
-        if (!config.selfContained) return false;
-        else if (config.selfContained === true) return true;
-        else return config.selfContained[flavor];
-    };
+  config.rocLogin = config.rocLogin || {};
 
-    config.rocLogin = config.rocLogin || {};
+  config.designDoc = config.designDoc || 'customApp';
 
-    config.designDoc = config.designDoc || 'customApp';
-
-    return config;
+  return config;
 };
-
--- src/filters.js
+++ src/filters.js
@@ -1,48 +1,55 @@
 'use strict';
 
-var path = require('path');
-var urlLib = require('url');
-var utils = require('./utils');
+let path = require('path');
+let urlLib = require('url');
+
+let utils = require('./utils');
 
 function filters(config) {
-    var plist = [];
+  let plist = [];
 
-    function concat(a, b) {
-        if (b === undefined) return a;
-        return a + b;
+  function concat(a, b) {
+    if (b === undefined) return a;
+    return a + b;
+  }
+
+  function processUrl(url, reldir, flavorName) {
+    if (!config.isSelfContained(flavorName)) {
+      // We return the original url
+      return url;
     }
 
-    function processUrl(url, reldir, flavorName) {
-        if (!config.isSelfContained(flavorName)) {
-            // We return the original url
-            return url;
-        }
+    url = utils.rewriteUrl(url);
+    plist.push(utils.cacheUrl(config, url, flavorName));
+    return utils.getLocalUrl(config, url, reldir);
+  }
 
-        url = utils.rewriteUrl(url);
-        plist.push(utils.cacheUrl(config, url, flavorName));
-        return utils.getLocalUrl(config, url, reldir);
-    }
+  function visualizer(version, reldir, flavorName) {
+    if (!config.isSelfContained(flavorName)) return null;
+    version = utils.checkVersion(version);
+    let visualizerUrl = `${config.cdn}/visualizer`.replace(/^\/\//, 'https://');
+    let parsedUrl = urlLib.parse(visualizerUrl);
 
-    function visualizer(version, reldir, flavorName) {
-        if (!config.isSelfContained(flavorName)) return null;
-        version = utils.checkVersion(version);
-        let visualizerUrl = (config.cdn + '/visualizer').replace(/^\/\//, 'https://');
-        let parsedUrl = urlLib.parse(visualizerUrl);
+    return path.join(
+      reldir,
+      config.libFolder,
+      parsedUrl.hostname,
+      parsedUrl.path,
+      version,
+    );
+  }
 
-        return path.join(reldir, config.libFolder, parsedUrl.hostname, parsedUrl.path, version);
-    }
+  let r = {
+    concat: concat,
+    processUrl: processUrl,
+    visualizer: visualizer,
+  };
 
-    var r = {
-        concat: concat,
-        processUrl: processUrl,
-        visualizer: visualizer
-    };
-
-    Object.defineProperty(r, 'plist', {
-        enumerable: false,
-        value: plist
-    });
-    return r;
+  Object.defineProperty(r, 'plist', {
+    enumerable: false,
+    value: plist,
+  });
+  return r;
 }
 
 exports = module.exports = filters;
